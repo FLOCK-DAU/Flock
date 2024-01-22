8a9c6f54ad65be79452e4d792113133f472b8160
채승지 <cotmdwl1405@gmail.com>
2024-01-23 01:44:02 +0900

    feat : OAuth 관련 설정 추가

--- back/Flock/src/main/java/com/Flock/domain/Member/Entity/MemberDetail.java
+++ back/Flock/src/main/java/com/Flock/domain/Member/Entity/MemberDetail.java
@@ -1,24 +1,40 @@
 package com.Flock.domain.Member.Entity;
 
+import com.Flock.domain.Member.DTO.MemberDto;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
+import java.util.Map;
 
-public class MemberDetail implements UserDetails {
+public class MemberDetail implements UserDetails, OAuth2User {
 
-    private Member member;
+    private MemberDto memberDto;
 
-    public MemberDetail(Member member){
-        this.member = member;
+    private Map<String,Object> oAuth2Attribute;
+
+    public MemberDetail(MemberDto memberDto){
+        this.memberDto = memberDto;
+        this.oAuth2Attribute = Map.of();
     }
+
+
+    // oAuth 인증할 때 사용
+    public MemberDetail(MemberDto memberDto,Map<String,Object> oAuth2Attribute){
+        this.memberDto = memberDto;
+        this.oAuth2Attribute = oAuth2Attribute;
+    }
+
+
+
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         ArrayList<GrantedAuthority> auth = new ArrayList<>();
-        auth.add(new SimpleGrantedAuthority(this.member.getRole().toString()));
+        auth.add(new SimpleGrantedAuthority(this.memberDto.getRole().toString()));
         return auth;
 
 //        ArrayList<GrantedAuthority> auths = new ArrayList<>();
@@ -36,12 +52,12 @@ public class MemberDetail implements UserDetails {
     // 비밀번호 정보 제공
     @Override
     public String getPassword() {
-        return member.getPassword();
+        return memberDto.getPassword();
     }
     // ID 정보 제공
     @Override
     public String getUsername() {
-        return member.getLoginId();
+        return memberDto.getLoginId();
     }
     // 계정 만료여부 제공
     // 특별히 사용을 안할시 항상 true를 반환하도록 처리
@@ -66,5 +82,25 @@ public class MemberDetail implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+
+    // OAuth에서 필요한 메소드
+
+    @Override
+    public String getName() {
+        return memberDto.getLoginId();
+    }
+
+    /**
+     * key value 값으로 어떤 값이든 받겠다
+     */
+    @Override
+    public Map<String, Object> getAttributes() {
+        return oAuth2Attribute;
+    }
+
+    public static MemberDetail from(MemberDto memberDto) {
+        return new MemberDetail(memberDto);
     }
 }

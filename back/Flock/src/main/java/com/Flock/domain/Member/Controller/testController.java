9ae5a1dd77eef96870dd8e49f74d07cb1341ee69
채승지 <cotmdwl1405@gmail.com>
2024-01-23 01:45:17 +0900

    feat : test용 컨트롤러 작성

---
+++ back/Flock/src/main/java/com/Flock/domain/Member/Controller/testController.java
@@ -0,0 +1,52 @@
+package com.Flock.domain.Member.Controller;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class testController {
+
+    @GetMapping("/test-login")
+    public String test(){
+        return "testLogin";
+    }
+
+    @GetMapping("/login/oauth2/code/kakao")
+    public ResponseEntity<?> getAccessToken(@RequestParam("code") String code){
+
+
+
+        // 1. header 생성
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded;charset=utf-8");
+
+        // 2. body 생성
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("grant_type", "authorization_code"); //고정값
+        params.add("client_id", "REST_API_KEY 입력");
+        params.add("redirect_uri", "http://localhost:8080/login/oauth2/code/kakao"); //등록한 redirect uri
+        params.add("code", code);
+
+        // 3. header + body
+        HttpEntity<MultiValueMap<String, String>> httpEntity = new HttpEntity<>(params, httpHeaders);
+
+        // 4. http 요청하기
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Object> response = restTemplate.exchange(
+                "https://kauth.kakao.com/oauth/token",
+                HttpMethod.POST,
+                httpEntity,
+                Object.class
+        );
+
+        return response;
+    }
+}

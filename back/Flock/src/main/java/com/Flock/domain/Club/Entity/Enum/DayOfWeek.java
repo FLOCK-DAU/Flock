217c50e9875adff505f7c510ae2bb9aa2bc1c4ea
채승지 <cotmdwl1405@gmail.com>
2024-01-25 23:56:16 +0900

    feat : 요일 리스트 변환

--- back/Flock/src/main/java/com/Flock/domain/Club/Entity/Enum/DayOfWeek.java
+++ back/Flock/src/main/java/com/Flock/domain/Club/Entity/Enum/DayOfWeek.java
@@ -1,5 +1,9 @@
 package com.Flock.domain.Club.Entity.Enum;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum DayOfWeek {
     MONDAY("월요일"),
     TUESDAY("화요일"),
@@ -13,5 +17,34 @@ public enum DayOfWeek {
 
     DayOfWeek(String description){
         this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public static DayOfWeek fromDescription(String description) {
+        for (DayOfWeek day : DayOfWeek.values()) {
+            if (day.getDescription().equals(description)) {
+                return day;
+            }
+        }
+        throw new IllegalArgumentException("No enum constant for description: " + description);
+    }
+
+    public static List<DayOfWeek> from(List<String> descriptions) {
+        List<DayOfWeek> dayOfWeeks = new ArrayList<>();
+
+        for (String description : descriptions) {
+            dayOfWeeks.add(fromDescription(description));
+        }
+
+        return dayOfWeeks;
+    }
+
+    public static String activityDaysToString(List<DayOfWeek> days) {
+        return days.stream()
+                .map(DayOfWeek::name)
+                .collect(Collectors.joining(","));
     }
 }
